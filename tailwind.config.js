8249f1a6f810f1ba65d3aa81713935e2ad601416
comolays27 <comolays27@uoc.edu>
2025-05-17 17:19:47 +0200

    index done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,scss}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        principal: ['var(--font-principal)'],
+      },
+      
+      colors: {
+        'common-background': '#d6cfec',
+        'nav-color': '#eca539',
+        'header-nav-links': '#94e284',
+        'nav-botones-hover': '#eca539',
+        'standar-blue': '#6b91f1',
+        'main-sentence': '#35f60a',
+      }
+    },
+  },
+  plugins: [],
+}
