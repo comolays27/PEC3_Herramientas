5826a970cef654b3bc9e2fcb5078d939677ac287
comolays27 <comolays27@uoc.edu>
2025-05-18 19:02:35 +0200

    postinclude done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        principal: ['var(--font-principal)'],
+        principal: ['"Roboto Flex"', 'sans-serif'],
       },
       
       boxShadow: {
